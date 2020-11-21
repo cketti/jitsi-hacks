96ca6640e1f1080cb2a37a7985921c439e3804e4
cketti <ck@cketti.de>
2020-11-21 16:46:25 +0100

    Update VIP script to persist VIP status and order

--- docs/vip.js
+++ docs/vip.js
@@ -4,6 +4,10 @@ if (!window.JitsiMeetJS) {
   console.log("[Jitsi Hacks] The script has already been loaded");
 } else (function() {
 
+  const orderOffset = -9999;
+  let order;
+  let vips;
+
   function addVipStyles() {
     $('head').append(`
       <style>
@@ -33,15 +37,31 @@ if (!window.JitsiMeetJS) {
   }
 
   function onDomChanged(mutationsList, observer) {
-    const targetNode = document.getElementsByClassName('filmstrip')[0];
-    const elements = targetNode.getElementsByClassName('popupmenu');
-    if (elements.length <= 0) return;
+    addVipEffectsIfNecessary();
+    addVipMenuItemIfNecessary();
+  }
 
-    const popupMenu = elements[0];
-    const menuItems = popupMenu.getElementsByClassName('jitsi-hacks-vip-menu-item');
-    if (menuItems.length > 0) return;
+  function addVipEffectsIfNecessary() {
+    $('.filmstrip span.videocontainer:not(#localVideoContainer)').each(function() {
+      const container = $(this);
+      if (container.css('order') == '0') {
+        const participant = getParticipantFromContainer(container);
+        if (participant) {
+          const displayName = participant.getDisplayName();
+          if (vips.has(displayName)) {
+            const order = vips.get(displayName);
+            addVipEffects(container, order);
+          }
+        }
+      }
+    });
+  }
 
-    const menuItem = $(`
+  function addVipMenuItemIfNecessary() {
+    const popupMenu = $('.filmstrip .popupmenu:not(:has(.jitsi-hacks-vip-menu-item))').first();
+    if (popupMenu.length == 0) return;
+
+    $(`
       <li class="popupmenu__item">
       <a class="popupmenu__link jitsi-hacks-vip-menu-item">
         <span class="popupmenu__icon">
@@ -50,35 +70,70 @@ if (!window.JitsiMeetJS) {
         <span class="popupmenu__text">Toggle VIP status</span>
       </a>
       </li>
-    `);
-
-    menuItem.click(onVipMenuItemClicked)
-
-    $(popupMenu).append(menuItem);
+    `)
+    .click(onVipMenuItemClicked)
+    .appendTo(popupMenu);
   }
-
-  let order = -9999;
 
   function onVipMenuItemClicked(event) {
     const container = $(event.target).parents('span.videocontainer').first();
-    const videoToolbar = container.find('.videocontainer__toolbar > div')
-    if (container.css('order') != '0') {
-      videoToolbar.find('.jitsi-hacks-vip-indicator').remove();
-      container.css('order', '0');
+    const participant = getParticipantFromContainer(container);
+    if (!participant) {
+      console.log("[Jitsi Hacks] Couldn't find participant with ID " + participantId);
+      return;
+    }
+
+    const displayName = participant.getDisplayName();
+    if (vips.has(displayName)) {
+      vips.delete(displayName);
+      removeVipEffects(container);
     } else {
-      videoToolbar.append('<div class="jitsi-hacks-vip-indicator">VIP</div>');
-      container.css('order', order);
+      vips.set(displayName, order);
+      addVipEffects(container, order);
       order++;
     }
 
-    //const participantId = container[0].id.replace('participant_', '');
-    //console.log("[Jitsi Hacks]: Toggled VIP status for: " + participantId);
+    saveVips();
   }
 
+  function getParticipantFromContainer(container) {
+    const participantId = container[0].id.replace('participant_', '');
+    return APP.conference.getParticipantById(participantId);
+  }
+
+  function removeVipEffects(container) {
+    container.find('.jitsi-hacks-vip-indicator').remove();
+    container.css('order', '0');
+  }
+
+  function addVipEffects(container, order) {
+    const videoToolbar = container.find('.videocontainer__toolbar > div');
+    videoToolbar.append('<div class="jitsi-hacks-vip-indicator">VIP</div>');
+    container.css('order', order + orderOffset);
+  }
+
+  function saveVips() {
+    const displayNames = [...vips].sort((first, second) => first[1] - second[1]).map(it => it[0]);
+    const json = JSON.stringify(displayNames);
+    window.localStorage.setItem("jitsiHacks/vip/vips", json);
+  }
+
+  function loadVips() {
+    const displayNames = JSON.parse(window.localStorage.getItem("jitsiHacks/vip/vips"));
+    if (displayNames) {
+      vips = new Map(displayNames.map((displayName, index) => [displayName, index]));
+    } else {
+      vips = new Map();
+    }
+    order = vips.size;
+  }
+
+  loadVips();
   addVipStyles();
   addVipMenuItem();
+  addVipEffectsIfNecessary();
 
-  window.jitsiHacksVip = "0.1";
+  window.jitsiHacksVip = "0.2";
 
   console.log("[Jitsi Hacks] VIP script loaded.");
 })();
