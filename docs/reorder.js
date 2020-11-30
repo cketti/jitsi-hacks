a9d4d27b144bf21465973ffc460b216196f56f09
cketti <ck@cketti.de>
2020-11-30 02:16:37 +0100

    Update 'reorder video tiles' script
    Handle Dragula's 'cancel' event. Don't allow reordering while a popup 
    (menu or connection status) is showing.

--- docs/reorder.js
+++ docs/reorder.js
@@ -41,17 +41,10 @@ function addDragulaStyles() {
 function setupDragAndDrop() {
     let videoTiles;
 
-    // Don't use jQuery's methods to remove nodes. That will get rid of Jitsi's event listeners.
-    dragula([], {
-        revertOnSpill: true,
-        mirrorContainer: $('#filmstripRemoteVideosContainer')[0],
-        isContainer: function (element) {
-            // Only allow reordering in tile view
-            return element.id == 'filmstripRemoteVideosContainer' && $(element).parents('.tile-view').length == 1;
-        }
-    }).on('drag', function (element, source) {
-        const jSource = $(source);
-        const children = jSource.children();
+    // Don't use jQuery functions to remove nodes. That will get rid of Jitsi's jQuery event listeners.
+    function rewriteDomToDisplayOrder(container) {
+        const jContainer = $(container);
+        const children = jContainer.children();
         videoTiles = children.toArray();
 
         // Remove video tiles from DOM
@@ -61,26 +54,48 @@ function setupDragAndDrop() {
         const sortedTiles = videoTiles.slice().sort((a,b) => parseInt(a.style.order, 10) - parseInt(b.style.order, 10));
 
         // Add video tile elements in display order to DOM
-        sortedTiles.forEach(videoTile => source.appendChild(videoTile));
+        sortedTiles.forEach(videoTile => container.appendChild(videoTile));
 
         // Set CSS 'order' properties for all video tiles to 0
-        jSource.children().css('order', '0');
-    }).on('drop', function (element, target, source, sibling) {
-        const jChildren = $(target).children();
+        jContainer.children().css('order', '0');
+    }
+
+    function restoreOriginalDomOrder(container) {
+        const jChildren = $(container).children();
         const numberOfVideos = jChildren.length;
 
         // Set CSS 'order' properties to reflect current DOM order = display order
         jChildren.each(function(index) {
-            $(this).css('order', -numberOfVideos - 1 + index);
+            $(this).css('order', -numberOfVideos + index);
         });
 
         // Remove video tiles from DOM
         videoTiles.forEach(videoTile => videoTile.remove());
 
         // Add video tiles to DOM in original order (now the CSS 'order' property is used for the display order)
-        videoTiles.forEach(videoTile => target.appendChild(videoTile));
-    });
+        videoTiles.forEach(videoTile => container.appendChild(videoTile));
 
+        videoTiles = null;
+    }
+
+    dragula([], {
+        revertOnSpill: true,
+        mirrorContainer: $('#filmstripRemoteVideosContainer')[0],
+        isContainer: function (element) {
+            // Only allow reordering in tile view
+            return element.id == 'filmstripRemoteVideosContainer' && $(element).parents('.tile-view').length == 1;
+        },
+        moves: function (element, source, handle, sibling) {
+            // Don't allow reordering while a popup is visible
+            return $(handle).parents('.popover').length == 0;
+        }
+    }).on('drag', function (element, source) {
+        rewriteDomToDisplayOrder(source);
+    }).on('drop', function (element, target, source, sibling) {
+        restoreOriginalDomOrder(target);
+    }).on('cancel', function (element, container, source) {
+        restoreOriginalDomOrder(container);
+    });
 }
 
 function addDragulaScript(callback) {
@@ -93,7 +108,7 @@ function addDragulaScript(callback) {
 addDragulaStyles();
 addDragulaScript(setupDragAndDrop);
 
-window.jitsiHacksReorder = "0.1";
+window.jitsiHacksReorder = "0.2";
 
 console.log("[Jitsi Hacks] Reorder script loaded.");
 })();
